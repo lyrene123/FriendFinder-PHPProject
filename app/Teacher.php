6990e0fe1ecdeba412e1931eadb8d069fbad6e11
Lyrene Labor <laborlyrene@gmail.com>
2017-11-21 23:28:28 +0000

    Added migration files for the course table, the teachers table, nd the courseSchedule table

--- app/Teacher.php
+++ app/Teacher.php
@@ -4,23 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Teacher extends Model
 {
-    protected $fillable = ["class", "title"];
+    protected $fillable = [
+        "name",
+    ];
 
     public function newPivot(self $parent, array $attributes, $table, $exists, $using = null)
     {
-        if($parent instanceof Teacher){
+        if($parent instanceof Course){
             return new CourseSchedule($parent, $attributes, $table, $exists);
         }
         return parent::newPivot($parent, $attributes, $table, $exists);
     }
 
-    public function teachers(){
+    public function courses(){
         return $this->belongsToMany("App\Course")->using("App\CourseSchedule");
-    }
-
-    public function users(){
-        return $this->belongsToMany("App\User")->using("App");
     }
 }
---
+++ database/migrations/2017_11_21_230006_create_courseSchedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courseSchedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('class');
+            $table->string('section');
+            $table->integer('day')->unsigned();
+            $table->integer('start')->unsigned();
+            $table->integer('end')->unsigned();
+            $table->integer('teacher_id')->unsigned();
+            $table->integer('course_id')->unsigned();
+            $table->foreign("teacher_id")->references("id")->on("teachers");
+            $table->foreign("course_id")->references("id")->on("courses");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courseSchedules');
+    }
+}
