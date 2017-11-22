b7e81dd6d452f135a6de2a35926446ffa106f1de
Lyrene Labor <laborlyrene@gmail.com>
2017-11-22 19:43:38 +0000

    Pengkim found a mistake we did not see when setting up the database. In the UserEnrollment, the table variable is missing an 's' after the word user. In the users_enrollment migration class, we forgot to remove the class field. So made the necessary changes and will to rollback local and heroku database and seed once again after merging into master branch.

--- app/Friend.php
+++ app/Friend.php
@@ -7,8 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Friend extends Model
 {
     protected $fillable = ['sender_id', 'receiver_id','confirmed',];
+    protected $table = 'friends';
 
     public function users() {
         return $this->belongsTo('App\User');
+    }
+
+    public function userCanUnfriend(User $user){
+
+        $userid
+        //return $user->id === $this->u
     }
 }
