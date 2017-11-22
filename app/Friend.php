da072bd504ad2f8db9b4a53c92e6a08045bc300e
Lyrene Labor <laborlyrene@gmail.com>
2017-11-22 20:36:21 +0000

    Started coding the Friend Model by adding the userCanUnfriend method which will check if a user is allowed to unfriend/delete a friend.

--- app/Friend.php
+++ app/Friend.php
@@ -4,18 +4,41 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Encapsulates the properties and behavior of a Friend Model in the FriendFinder application.
+ * Represents the Friend entity of the database.
+ *
+ * @author Lyrene Labor
+ * @author Pengkim Sy
+ * @author Peter Bellefleur
+ * @author Phil Langloid
+ * @package App
+ */
 class Friend extends Model
 {
     protected $fillable = ['sender_id', 'receiver_id','confirmed',];
     protected $table = 'friends';
 
+    /**
+     * Associates a User with Friend entry.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function users() {
         return $this->belongsTo('App\User');
     }
 
+    /**
+     * Checks if a User can unfriend current friend entry. A User cannot unfriend
+     * someone if User has no relation whatsoever with that person in the Friend table.
+     *
+     * @param User $user a User instance to verify
+     * @return bool true or false if input User can unfriend
+     */
     public function userCanUnfriend(User $user){
-
-        $userid
-        //return $user->id === $this->u
+        if($user->id === $this->sender_id || $user->id === $this->receiver_id){
+            return true;
+        }
+        return false;
     }
 }
