85def0a6450c8162075f7ffc2c452aa018d82ce0
Vivshi <planglois.pedroso@gmail.com>
2017-12-01 20:18:40 +0000

    Added comments and cleaned the code a tad

--- app/Http/Controllers/CourseManagerController.php
+++ app/Http/Controllers/CourseManagerController.php
@@ -5,18 +5,18 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Course;
 use App\CourseUser;
-use App\Teacher;
-use App\Friend;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 
+/**
+ * Controller the manager the course manager page.
+ */
 class CourseManagerController extends Controller
 {
     /**
-     *
+     * Creates an instance of the controller.
      *
      * CourseManagerController constructor.
      */
@@ -25,13 +25,14 @@ class CourseManagerController extends Controller
     }
 
     /**
-     *
+     * Method that associates the index view with it's associated models.
      *
      * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index(Request $request) {
 
+        // Get all courses the user is currently registered in
         $registered_courses = User::find(Auth::user()->id)
             ->where("users.id", "=", Auth::user()->id)
             ->join("course_user", "course_user.user_id", "=", "users.id")
@@ -44,13 +45,14 @@ class CourseManagerController extends Controller
     }
 
     /**
-     *
+     * Associates the search view with it's associated models.
      *
      * @param Request $request
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function search(Request $request){
 
+        // Get all courses the user is currently registered in
         $registered_courses = User::find(Auth::user()->id)
             ->where("users.id", "=", Auth::user()->id)
             ->join("course_user", "course_user.user_id", "=", "users.id")
@@ -59,14 +61,17 @@ class CourseManagerController extends Controller
 
         $user = User::where("users.id", "=", Auth::user()->id)->get();
 
+        // validate the user input
         $this->validate($request, ['search_input' => 'required',]);
         $input = $request->input("search_input");
 
         $courses = array();
 
+        // find the courses associated with the user input
         $courses_by_name = Course::where('class', 'like', '%'.$input.'%')->get();
         $courses_by_title = Course::where('title', 'like', '%'.$input.'%')->get();
 
+        // courses information by course name
         foreach($courses_by_name as $cbn){
             if(!in_array($cbn, $courses)){
                 if($registered_courses->where('title', $cbn->title)->count() === 0){
@@ -81,6 +86,7 @@ class CourseManagerController extends Controller
             }
         }
 
+        // courses information by course title
         foreach($courses_by_title as $cbt){
             if(!in_array($cbt, $courses)){
                 if($registered_courses->where('title', $cbt->title)->count() === 0){
@@ -95,8 +101,8 @@ class CourseManagerController extends Controller
             }
         }
 
+        // courses information by course teacher's name
        $allCourses = Course::all();
-       $courses_teacher = array();
        foreach ($allCourses as $aCourse){
            $found = $aCourse->teachers()
                         ->where('name','like', "%$input%")
@@ -121,7 +127,7 @@ class CourseManagerController extends Controller
     }
 
     /**
-     *
+     *  Associate the delete view with it's model
      *
      * @param Request $request
      * @param Course $course
@@ -140,7 +146,7 @@ class CourseManagerController extends Controller
     }
 
     /**
-     *
+     *  Associate the add view with it's model
      *
      * @param Request $request
      * @param Course $course
