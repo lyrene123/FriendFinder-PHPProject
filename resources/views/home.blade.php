28df3071239c94a81ed8f5f473b82d05f343f543
P Bellefleur <bellefleurpeter@yahoo.ca>
2017-12-04 13:57:45 -0500

    Display list of friends on home page
    Home page will display a user's list of friends, as well as links to
    pages for managing and finding friends.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,36 +16,75 @@
                         </div>
                     @endif
 
-                @if(isset($reistered_courses) && count($registered_courses) > 0)
-                   <table class="table table-striped task-table">
-                         <thead>
-                            <th>Class ID</th>
-                            <th>Section</th>
-                            <th>Title</th>
-                         </thead>
-                         <tbody>
-                         @foreach($registered_courses as $course)
-                             <tr>
-                                 <td>{{ $course->class }}</td>
-                                 <td>{{ $course->section }}</td>
-                                 <td>{{ $course->title }}</td>
-                             </tr>
-                         @endforeach
-                         </tbody>
-                   </table>
-                @else
-                    <p class="text-center">You have not registered for any courses.</p>
-                @endif
+                    @if(isset($registered_courses) && count($registered_courses) > 0)
+                        <table class="table table-striped task-table">
+                            <thead>
+                                <th>Class ID</th>
+                                <th>Section</th>
+                                <th>Title</th>
+                            </thead>
+                            <tbody>
+                            @foreach($registered_courses as $course)
+                                <tr>
+                                    <td>{{ $course->class }}</td>
+                                    <td>{{ $course->section }}</td>
+                                    <td>{{ $course->title }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                            <p class="text-center">You have not registered for any courses.</p>
+                    @endif
                     @if (Auth::check())
                         <section class="text-center">
                             <a href=" {{ route('coursemanager') }}">Manage your courses</a>
+                            <br/>
                         </section>
                     @endif
-                </div>
-                <div>
+
+                    @if(isset($friends) && count($friends) > 0)
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <table class="table table-striped task-table">
+                                    <thead>
+                                        <th>Friends</th>
+                                        <th></th>
+                                    </thead>
+                                    <tbody>
+                                    @foreach ($friends as $friend)
+                                        <tr>
+                                            <td class="table-text">
+                                                <strong>{{ $friend['firstname'] }}</strong>
+                                                <strong>{{ $friend['lastname'] }}</strong>
+                                            </td>
+
+                                            @if($friend['confirmed'] === true)
+                                                <td>
+                                                    <strong>CONFIRMED</strong>
+                                                </td>
+                                            @else
+                                                <td>
+                                                    <strong>PENDING</strong>
+                                                </td>
+                                            @endif
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                {!! $friends->render() !!}
+                            </div>
+                        </div>
+                    @else
+                        <div class="panel-heading">
+                            You have no registered friends.
+                        </div>
+                    @endif
                     @if (Auth::check())
                         <section class="text-center">
                             <a href=" {{ route('friends') }} ">Manage your friends</a>
+                            <br/>
+                            <a href=" {{ route('friendbreak') }}">Find friends on break</a>
                         </section>
                     @endif
                 </div>
