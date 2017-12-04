3ecaa480abe42bb97af598f7fa7a838e360898a5
P Bellefleur <bellefleurpeter@yahoo.ca>
2017-12-04 13:26:48 -0500

    Display registered courses on home page
    A registered user viewing the home page will see a list of courses they
    are currently registered for. If no courses are registered, they will
    instead see a message indicating this.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Friend;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class HomeController
@@ -28,6 +31,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $registered_courses = User::find(Auth::user()->id)
+            ->where("users.id", "=", Auth::user()->id)
+            ->join("course_user", "course_user.user_id", "=", "users.id")
+            ->join("courses", "courses.id", "=", "course_user.course_id")
+            ->get();
+
+        $user = User::where("users.id", "=", Auth::user()->id)->get();
+
+        return view('home', ['registered_courses' => $registered_courses, 'user' => $user,]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">
+                    <h3 class="text-center">Hello, {{ $user[0]->firstname }}!</h3>
+                </div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,12 +16,37 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                @if(isset($reistered_courses) && count($registered_courses) > 0)
+                   <table class="table table-striped task-table">
+                         <thead>
+                            <th>Class ID</th>
+                            <th>Section</th>
+                            <th>Title</th>
+                         </thead>
+                         <tbody>
+                         @foreach($registered_courses as $course)
+                             <tr>
+                                 <td>{{ $course->class }}</td>
+                                 <td>{{ $course->section }}</td>
+                                 <td>{{ $course->title }}</td>
+                             </tr>
+                         @endforeach
+                         </tbody>
+                   </table>
+                @else
+                    <p class="text-center">You have not registered for any courses.</p>
+                @endif
+                    @if (Auth::check())
+                        <section class="text-center">
+                            <a href=" {{ route('coursemanager') }}">Manage your courses</a>
+                        </section>
+                    @endif
                 </div>
                 <div>
                     @if (Auth::check())
-                        <section><a href=" {{ route('friends') }} ">Manage your friends</a></section>
-                        <section><a href=" {{ route('coursemanager') }}">Manage your courses</a></section>
+                        <section class="text-center">
+                            <a href=" {{ route('friends') }} ">Manage your friends</a>
+                        </section>
                     @endif
                 </div>
             </div>
