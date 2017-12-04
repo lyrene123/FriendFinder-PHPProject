06ce34ab6f1718dbc92433fb601093628f03b77b
P Bellefleur <bellefleurpeter@yahoo.ca>
2017-12-04 13:39:07 -0500

    Retrieve list of friends in HomeController
    HomeController retrieves a list of registered friends from the database,
    and creates a LengthAwarePaginator to store them.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Friend;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 
 /**
@@ -31,6 +32,22 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $friends = User::find(Auth::user()->id)
+            ->friends()
+            ->get();
+
+        $result_friends = array();
+        foreach($friends as $friend) {
+            $item = array();
+            $item['id'] = $friend->id;
+            $item['confirmed'] = $friend->confirmed;
+            $user = User::find($friend->receiver_id);
+            $item['firstname'] = $user->firstname;
+            $item['lastname'] = $user->lastname;
+            $result_friends[] = $item;
+        }
+
+        $friends_paginated = $this->constructPagination($result_friends, 5);
 
         $registered_courses = User::find(Auth::user()->id)
             ->where("users.id", "=", Auth::user()->id)
@@ -40,6 +57,24 @@ class HomeController extends Controller
 
         $user = User::where("users.id", "=", Auth::user()->id)->get();
 
-        return view('home', ['registered_courses' => $registered_courses, 'user' => $user,]);
+        return view('home', ['friends' => $friends_paginated, 'registered_courses' => $registered_courses, 'user' => $user,]);
+    }
+
+    /**
+     * Handles pagination for a given data array, and with a number of records per page to display.
+     * Code based on tutorial from Mahdi Hazaveh:
+     * http://blog.hazaveh.net/2016/03/laravel-5-manual-pagination-from-array/
+     *
+     * @param $array An array of data to paginate.
+     * @param $numRecords   The number of records to show, per page.
+     * @return LengthAwarePaginator
+     */
+    private function constructPagination($array, $numRecords) {
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $col = new Collection($array);
+        $perPage = $numRecords;
+        $entries = new LengthAwarePaginator($col->forPage($currentPage, $perPage), $col->count(), $perPage, $currentPage);
+        $entries->setPath(LengthAwarePaginator::resolveCurrentPath());
+        return $entries;
     }
 }
